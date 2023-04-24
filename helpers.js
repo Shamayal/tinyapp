31b74a29a7535d51e8c3cc321abb3aab9aaf563b
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-24 12:29:55 -0400

    added comments to helper functions

--- helpers.js
+++ helpers.js
@@ -3,6 +3,7 @@ const generateRandomString = () => {
   const alphanumericCharacters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUyVvWwXxYyZz0123456789';
   let result = '';
   for (let i = 1; i <= 6; i++) {
+    // randomly selects an index of the character and adds that letter to the string
     result += alphanumericCharacters.charAt(Math.floor(Math.random() * alphanumericCharacters.length));
   }
   return result;
@@ -11,6 +12,7 @@ const generateRandomString = () => {
 // look up user by email address
 const getUserByEmail = function(email, users) {
   for (let user in users) {
+    // loops through users to find the object value that equals the email passed as the argument
     if (users[user].email === email) {
       return users[user];
     }
@@ -22,7 +24,9 @@ const getUserByEmail = function(email, users) {
 const urlsForUser = function(id, data) {
   let userURLs = {};
   for (let tinyLink in data) {
+    // loops through the database to find a user id that matches the id passed as the argument
     if (data[tinyLink].userID === id) {
+      // match found, shortURL and longURL key-value pair added to object
       userURLs[tinyLink] = data[tinyLink].longURL;
     }
   }
